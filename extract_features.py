e5f70cdce362be2756fa4a59e7f42b11d5502d83
Eva Schlinger <eschling@andrew.cmu.edu>
2014-02-14 00:13:06 -0500

    add a/b relation features and cluster features

--- extract_features.py
+++ extract_features.py
@@ -11,9 +11,12 @@ def main():
   parser.add_argument('-r','--response',help='output file for responses',required=False)
   parser.add_argument('-t','--training',help='file of responses and word pairs for classification')
   parser.add_argument('--test', action='store_true',help='features only, no response file produced')
+  parser.add_argument('-c','--cluster',help='source as brown clusters')
+  parser.add_argument('-e','--extra', action='store_true',help='for each feature, have two versions: one which explicitly contains the relationship between a and b, and one that does not')
   args = parser.parse_args()
 
   source_file = open(args.source)
+  cluster_file = open(args.cluster)
   dependency_file = open(args.dependency)
   feature_file = open(args.feature, 'w')
   training = open(args.training)
@@ -22,23 +25,24 @@ def main():
   instance = 0
   source_line = -1
   source = []
-  dependency = dict()
+  clusters = []
   dep_line = 0
-
+  dependency = dict()
   for line in training:
     label, sent_num, a_index, b_index = line.split('\t')
     sent_num, a_index, b_index = int(sent_num), int(a_index), int(b_index)
     while sent_num > source_line: 
       src = source_file.readline().strip()
       source = src.split()
+      clusters = cluster_file.readline().strip().split()
       source_line +=1
-      dependecy = dict()
       dependency['token']=[]
       dependency['pos']=[]
       dependency['head']=[]
       dependency['mod']=[]
-      #print source_line, dep_line, a_index, b_index, instance
+      print source_line, dep_line, a_index, b_index, instance
       parse = dependency_file.readline().strip()
+      print parse
       while parse:
         conll = parse.split('\t')
         dependency['token'].append(conll[1])
@@ -60,7 +64,7 @@ def main():
     if not args.test:
       response_file.write('{}\t{}\n'.format(instance,label))
 
-    features = extract(source, a_index, b_index, dependency)    
+    features = extract(source, clusters, a_index, b_index, dependency, args.extra)    
     json_feat = json.dumps(features)    
     feature_file.write('{}\t{}\n'.format(instance,json_feat))
     instance += 1
@@ -69,12 +73,30 @@ def main():
   source_file.close()
   response_file.close()
   feature_file.close()
+
+def add_feature(features, new_feature, val, relation, add_relation=False):
+  features[new_feature] = val
+  features[new_feature[0]+relation+new_feature[1:]] = val
   
-def extract(source, a_index, b_index, dependency):
+def extract(source, clusters, a_index, b_index, dependency, add_relation=False):
   features = dict()
+  a_relation = ''
+  b_relation = ''
+  if dependency['head'][a_index]==dependency['head'][b_index]:
+    a_relation = 'a_sibling_of_b'
+    b_relation = 'b_sibling_of_a'
+  elif dependency['head'][a_index]==b_index:
+    a_relation = 'a_child_of_b'
+    b_relation = 'b_parent_of_a'
+  elif dependency['head'][b_index]==a_index:
+    a_relation = 'a_parent_of_b'
+    b_relation = 'b_child_of_a'
+
   features['gap_size'] = b_index - a_index - 1
   features['a_token_'+source[a_index]] = 1
   features['b_token_'+source[b_index]] = 1
+  features['a_cluster_'+clusters[a_index]] = 1
+  features['b_cluster_'+clusters[b_index]] = 1
   features['a_pos_'+dependency['pos'][a_index]] = 1
   features['b_pos_'+dependency['pos'][b_index]] = 1
   features['a_num_siblings'] = dependency['head'].count(dependency['head'][a_index]) - 1
@@ -83,24 +105,28 @@ def extract(source, a_index, b_index, dependency):
     features['a_root'] = 1
     b_parent = dependency['head'][b_index]
     features['b_head_token_'+source[b_parent]] = 1
+    features['b_head_cluster_'+clusters[b_parent]] = 1
     features['b_head_pos_'+dependency['pos'][b_parent]] = 1
     features['b_head_mod_'+dependency['mod'][b_parent]] = 1
     if not dependency['head'][b_parent] < 0:
       b_grand = dependency['head'][b_parent]
       features['b_grandparent_token_'+source[b_grand]] = 1
+      features['b_grandparent_cluster_'+clusters[b_grand]] = 1
       features['b_grandparent_pos_'+dependency['pos'][b_grand]] = 1
       features['b_grandparent_mod_'+dependency['mod'][b_grand]] = 1
   elif dependency['head'][b_index] < 0:
     features['b_root'] = 1
     a_parent = dependency['head'][a_index]
     features['a_head_token_'+source[a_parent]] = 1
+    features['a_head_cluster_'+clusters[a_parent]] = 1 
     features['a_head_pos_'+dependency['pos'][a_parent]] = 1
     features['a_head_mod_'+dependency['mod'][a_parent]] = 1
     if not dependency['head'][a_parent] < 0:
       a_grand = dependency['head'][a_parent]
-      features['b_grandparent_token_'+source[a_grand]] = 1
-      features['b_grandparent_pos_'+dependency['pos'][a_grand]] = 1
-      features['b_grandparent_mod_'+dependency['mod'][a_grand]] = 1
+      features['a_grandparent_token_'+source[a_grand]] = 1
+      features['a_grandparent_cluster_'+clusters[a_grand]] = 1
+      features['a_grandparent_pos_'+dependency['pos'][a_grand]] = 1
+      features['a_grandparent_mod_'+dependency['mod'][a_grand]] = 1
   else:
     if dependency['head'][b_index]==dependency['head'][a_index]:
       features['same_head'] = 1
@@ -110,29 +136,36 @@ def extract(source, a_index, b_index, dependency):
       features['b_head_of_a'] = 1
     b_parent = dependency['head'][b_index]
     features['b_head_token_'+source[b_parent]] = 1
+    features['b_head_cluster_'+clusters[b_parent]] = 1
     features['b_head_pos_'+dependency['pos'][b_parent]] = 1
     features['b_head_mod_'+dependency['mod'][b_parent]] = 1
     if not dependency['head'][b_parent] < 0:
       b_grand = dependency['head'][b_parent]
-      features['b_head_token_'+source[b_grand]] = 1
-      features['b_head_pos_'+dependency['pos'][b_grand]] = 1
-      features['b_head_mod_'+dependency['mod'][b_grand]] = 1
+      features['b_grandparent_token_'+source[b_grand]] = 1
+      features['b_grandparent_cluster_'+clusters[b_grand]] = 1
+      features['b_grandparent_pos_'+dependency['pos'][b_grand]] = 1
+      features['b_grandparent_mod_'+dependency['mod'][b_grand]] = 1
     a_parent = dependency['head'][a_index]
-    features['a_head_token_'+source[b_parent]] = 1
+    features['a_head_token_'+source[a_parent]] = 1
+    features['a_head_cluster_'+clusters[b_parent]] = 1
     features['a_head_pos_'+dependency['pos'][a_parent]] = 1
     features['a_head_mod_'+dependency['mod'][a_parent]] = 1
     if not dependency['head'][a_parent] < 0:
       a_grand = dependency['head'][a_parent]
-      features['b_head_token_'+source[a_grand]] = 1
-      features['b_head_pos_'+dependency['pos'][a_grand]] = 1
-      features['b_head_mod_'+dependency['mod'][a_grand]] = 1
+      features['a_grandparent_token_'+source[a_grand]] = 1
+      features['a_grandparent_cluster_'+clusters[a_grand]] = 1
+      features['a_grandparent_pos_'+dependency['pos'][a_grand]] = 1
+      features['a_grandparent_mod_'+dependency['mod'][a_grand]] = 1
   if (b_index - a_index) == 2:
     features['between_token_'+source[a_index+1]] = 1
+    features['between_cluster_'+clusters[a_index+1]] = 1
     features['between_pos_'+dependency['pos'][a_index+1]] = 1
   elif (b_index - a_index) > 2:
     features['between_a_token_'+source[a_index+1]] = 1
+    features['between_a_cluster_'+clusters[a_index+1]] = 1 
     features['between_a_pos_'+dependency['pos'][a_index+1]] = 1
     features['between_b_token_'+source[b_index-1]] = 1
+    features['between_b_cluster_'+clusters[b_index-1]] = 1
     features['between_b_pos_'+dependency['pos'][b_index-1]] = 1
  
   a_child = 0
@@ -142,6 +175,7 @@ def extract(source, a_index, b_index, dependency):
     if dependency['head'][i]==a_index:
       a_child += 1
       features['a_child_'+mod+'_token_'+source[i]] = 1
+      features['a_child_'+mod+'_cluster_'+clusters[i]] = 1 
       features['a_child_'+mod+'_pos_'+dependency['pos'][i]] = 1
       if i < a_index:
         if (a_index-i)==1:
@@ -158,6 +192,7 @@ def extract(source, a_index, b_index, dependency):
     elif dependency['head'][i]==b_index:
       b_child += 1
       features['b_child_'+mod+'_token_'+source[i]] = 1
+      features['b_child_'+mod+'_cluster_'+clusters[i]] = 1
       features['b_child_'+mod+'_pos_'+dependency['pos'][i]] = 1
 
       if i < b_index:
@@ -172,6 +207,13 @@ def extract(source, a_index, b_index, dependency):
           features['b_child_'+mod+'_after'] = 1
   features['a_num_children'] = a_child
   features['b_num_children'] = b_child
+ 
+  if add_relation and (a_relation or b_relation):
+    for f in features.keys():
+      if f[0:2]=='a_' and a_relation:
+        features[a_relation+f[1:]] = features[f]
+      elif f[0:2]=='b_' and b_relation:
+        features[b_relation+f[1:]] = features[f]
   return features
 
 if __name__=='__main__':
